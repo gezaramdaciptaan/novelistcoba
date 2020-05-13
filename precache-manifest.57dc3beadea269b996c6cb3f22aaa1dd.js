78976734168c4e4f0b4318fefbcccf6aed380246
gezaramdaciptaan <gezaramdaciptaan@gmail.com>
2020-05-13 13:40:44 +0700

    Updates

---
+++ precache-manifest.57dc3beadea269b996c6cb3f22aaa1dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8abe0db53ce0056d4c202fff4e036c3",
+    "url": "/coba/index.html"
+  },
+  {
+    "revision": "8669b80cfe2087d99f5a",
+    "url": "/coba/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "011dd63290ef3f617660",
+    "url": "/coba/static/css/main.f4140f89.chunk.css"
+  },
+  {
+    "revision": "8669b80cfe2087d99f5a",
+    "url": "/coba/static/js/2.fe92455f.chunk.js"
+  },
+  {
+    "revision": "494ff15427fc00bc6d2909d429ff11b9",
+    "url": "/coba/static/js/2.fe92455f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011dd63290ef3f617660",
+    "url": "/coba/static/js/main.8ef18181.chunk.js"
+  },
+  {
+    "revision": "735c9455aff4ee1bcea3",
+    "url": "/coba/static/js/runtime-main.6d5cf619.js"
+  }
+]);
